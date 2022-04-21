b7dfd1df635789d25b3c70ad9000e66260f5c490
holawan <asdf134652@gmail.com>
2022-04-21 16:59:16 +0900

    [ADD] artist_list view

--- Music/music/urls.py
+++ Music/music/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('artists/',views.artist_list)
 ]
--- Music/music/views.py
+++ Music/music/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Artist
+from .serializer import ArtistListSerializet
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def artist_list(request) :
+    artists = get_list_or_404(Artist)
+    serializer = ArtistListSerializet(artists,many=True)
+    return Response(serializer.data)
