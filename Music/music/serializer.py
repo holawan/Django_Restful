cb7e8a551316f3f0ff9686b866e96220b9b9ee3e
holawan <asdf134652@gmail.com>
2022-04-21 22:15:25 +0900

    [ADD] Music Serializer

--- Music/music/serializer.py
+++ Music/music/serializer.py
@@ -1,6 +1,6 @@
 from nntplib import ArticleInfo
 from rest_framework import serializers
-from .models import Artist
+from .models import Artist,Music
 
 
 class ArtistListSerializer(serializers.ModelSerializer) :
@@ -13,4 +13,10 @@ class ArtistSerializer(serializers.ModelSerializer) :
     
     class Meta :
         model = Artist
+        fields = '__all__'
+
+class MusicSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        model = Music
         fields = '__all__'
