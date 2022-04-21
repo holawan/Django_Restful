6951aa1ac74cbd01e819e0b9ade56bca8ad552d6
holawan <asdf134652@gmail.com>
2022-04-21 16:52:03 +0900

    [ADD] admin page custom

--- Music/music/admin.py
+++ Music/music/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Music,Artist
+
+class ArtistAdmin(admin.ModelAdmin) :
+    list_display = ('name',)
+admin.site.register(Artist,ArtistAdmin)
+
+class MusicAdmin(admin.ModelAdmin) :
+    list_display = ('artist', 'title')
+admin.site.register(Music,MusicAdmin)
