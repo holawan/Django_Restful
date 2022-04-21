d501c4c8ce33189dcc2ad3a8d47553b76208bf6a
holawan <asdf134652@gmail.com>
2022-04-21 15:25:18 +0900

    [ADD] 댓글의 수를 카운트할 필드 추가

--- articles/serializer.py
+++ articles/serializer.py
@@ -17,6 +17,8 @@ class CommentSerializer(serializers.ModelSerializer) :
 
 class ArticleSerializer(serializers.ModelSerializer) :
     comment_set = CommentSerializer(many=True,read_only = True)
+    #article.comment_set.count()를 가져오고 싶기 때문에 article이하를 source에 넣어줌 
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
     class Meta:
         model = Article
         fields = '__all__'
