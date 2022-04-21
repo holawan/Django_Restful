ea69d0b8e58c02e398253b0e8ee1c34771ab3fbb
holawan <asdf134652@gmail.com>
2022-04-21 13:55:15 +0900

    [ADD] Comment model

--- articles/serializer.py
+++ articles/serializer.py
@@ -1,5 +1,6 @@
+
 from rest_framework import serializers
-from .models import Article
+from .models import Article,Comment
 
 class ArticleListSerializer(serializers.ModelSerializer) :
 
@@ -11,4 +12,10 @@ class ArticleSerializer(serializers.ModelSerializer) :
 
     class Meta:
         model = Article
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        model = Comment
         fields = '__all__'
