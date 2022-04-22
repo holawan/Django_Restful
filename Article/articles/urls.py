e479d2aedc1c025ad4d5f6ee397e705209fc185c
holawan <asdf134652@gmail.com>
2022-04-22 12:29:49 +0900

    [update]swagger

--- Article/articles/urls.py
+++ Article/articles/urls.py
@@ -7,8 +7,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+    #필수인자
+      title="Snippets API Practice",
       default_version='v1',
+    
+    #아래부터는 선택인자 
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
@@ -25,5 +28,5 @@ urlpatterns = [
     path('cards/',views.card_list),
     path('cards/<int:card_pk>/' ,views.card_detail),
     path('<int:card_pk>/register/<int:article_pk>/', views.register),
-    path('swegger/', schema_view.with_ui('swagger')),
+    path('swagger/', schema_view.with_ui('swagger')),
 ]
