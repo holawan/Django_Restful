a3f0289878bfec305773d6b5c439a99e9c0952b2
holawan <asdf134652@gmail.com>
2022-04-22 10:38:29 +0900

    [UDPATE] many to many fields in serializer

--- Article/articles/serializer/article.py
+++ Article/articles/serializer/article.py
@@ -1,5 +1,7 @@
 
 from rest_framework import serializers
+
+from articles.serializer.card import CardSerializer
 from ..models import Article
 from .comment import CommentSerializer
 
@@ -8,6 +10,7 @@ class ArticleSerializer(serializers.ModelSerializer) :
     comment_set = serializers.PrimaryKeyRelatedField(many=True,read_only = True)
     #article.comment_set.count()를 가져오고 싶기 때문에 article이하를 source에 넣어줌 
     comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+    cards = CardSerializer(many=True,read_only=True)
     class Meta:
         model = Article
         fields = '__all__'
